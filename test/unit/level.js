c7b5917280fee74a7aee755c798c1af246102a30
Daniel Fields <daniel.s.fields@gmail.com>
2017-11-24 12:44:49 -0500

    Completing unit tests for Level.

--- test/unit/level.js
+++ test/unit/level.js
@@ -8,72 +8,105 @@ const Level = require('../../lib/level');
 describe('Level', function() {
 
   describe('#isValid', () => {
-    it('should return true if value 0', function() {
+    it('returns true if value 0', function() {
       assert.isTrue(Level.isValid(0));
     });
 
-    it('should return true if value 1', function () {
+    it('returns true if value 1', function () {
       assert.isTrue(Level.isValid(1));
     });
 
-    it('should return true if value 2', function () {
+    it('returns true if value 2', function () {
       assert.isTrue(Level.isValid(2));
     });
 
-    it('should return true if value 3', function () {
+    it('returns true if value 3', function () {
       assert.isTrue(Level.isValid(3));
     });
 
-    it('should return true if value 4', function () {
+    it('returns true if value 4', function () {
       assert.isTrue(Level.isValid(4));
     });
 
-    it('should return true if value 5', function () {
+    it('returns true if value 5', function () {
       assert.isTrue(Level.isValid(5));
     });
 
-    it('should return true if value 6', function () {
+    it('returns true if value 6', function () {
       assert.isTrue(Level.isValid(6));
     });
 
-    it('should return true if value 7', function () {
+    it('returns true if value 7', function () {
       assert.isTrue(Level.isValid(0));
     });
 
-    it('should return true if value emerg', function () {
+    it('returns true if value emerg', function () {
       assert.isTrue(Level.isValid('emerg'));
     });
 
-    it('should return true if value alert', function () {
+    it('returns true if value alert', function () {
       assert.isTrue(Level.isValid('alert'));
     });
 
-    it('should return true if value crit', function () {
+    it('returns true if value crit', function () {
       assert.isTrue(Level.isValid('crit'));
     });
 
-    it('should return true if value err', function () {
+    it('returns true if value err', function () {
       assert.isTrue(Level.isValid('err'));
     });
 
-    it('should return true if value warning', function () {
+    it('returns true if value warning', function () {
       assert.isTrue(Level.isValid('warning'));
     });
 
-    it('should return true if value notice', function () {
+    it('returns true if value notice', function () {
       assert.isTrue(Level.isValid('notice'));
     });
 
-    it('should return true if value info', function () {
+    it('returns true if value info', function () {
       assert.isTrue(Level.isValid('info'));
     });
 
-    it('should return true if value info', function () {
+    it('returns true if value info', function () {
       assert.isTrue(Level.isValid('info'));
     });
 
-    it('should return false if value is invalid', function () {
+    it('returns false if value is invalid', function () {
       assert.isFalse(Level.isValid(42));
+    });
+  });
+
+
+  describe('#assert', function() {
+    it('throws if invalid numeric log level', function() {
+      assert.throws(() => {
+        Level.assert(42);
+      }, TypeError);
+    });
+
+    it('throws if invalid keyword log level', function () {
+      assert.throws(() => {
+        Level.assert('blah');
+      }, TypeError);
+    });
+
+    it('does not throw if valid numeric log level', function() {
+      assert.doesNotThrow(() => {
+        Level.assert(3);
+      });
+    });
+
+    it('does not throw if valid keyword log level', function() {
+      assert.doesNotThrow(() => {
+        Level.assert('crit');
+      });
+    });
+
+    it('throws if given non-numeric or string', function() {
+      assert.throws(() => {
+        Level.assert({});
+      }, TypeError);
     });
   });
 
