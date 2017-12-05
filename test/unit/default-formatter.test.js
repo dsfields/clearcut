d33b9fedeea59622d180c804e1f291de2ee9c80d
Daniel Fields <daniel.s.fields@gmail.com>
2017-12-04 22:17:24 -0500

    Adding DefaultFormatter test stub.

---
+++ test/unit/default-formatter.test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { assert } = require('chai');
+
+const DefaultFormatter = require('../../lib/default-formatter');
+
+
+describe('DefaultFormatter', function() {
+
+  describe('.constructor', function() {
+
+  });
+
+
+  describe('#clone', function () {
+
+  });
+
+
+  describe('#format', function () {
+
+  });
+
+
+  describe('#supplement', function () {
+
+  });
+
+
+  describe('#terminate', function () {
+
+  });
+
+});
